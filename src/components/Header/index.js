b05ca980d4f2500ea3f48e85b24b98f5b4d9376d
Buianov Arkadiy <buianov@mail.ru>
2019-10-16 21:45:05 +0300

    finished with header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { HeaderContainer } from './index.style';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const noUnderline = {
+    textDecoration: 'none',
+  };
+
   return (
     <HeaderContainer>
-      <Button variant="contained" color="primary" onClick={() => {}}>
-        На главную
-      </Button>
-      <Button variant="contained" color="primary" onClick={() => {}}>
-        Новости
-      </Button>
-      <Button variant="contained" color="primary" onClick={() => {}}>
-        Профиль
-      </Button>
+      <Link to="/" style={noUnderline}>
+        <Button variant="contained" color="primary">
+          На главную
+        </Button>
+      </Link>
+      <Link to="/news" style={noUnderline}>
+        <Button variant="contained" color="primary">
+          Новости
+        </Button>
+      </Link>
+      <Link to="/profile" style={noUnderline}>
+        <Button variant="contained" color="primary">
+          Профиль
+        </Button>
+      </Link>
     </HeaderContainer>
   );
 };
