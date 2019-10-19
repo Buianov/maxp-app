36243cb00a1450b82433ba30e9f8e32b04df11df
Buianov Arkadiy <buianov@mail.ru>
2019-10-19 20:47:26 +0300

    rewrite newspage component to function component with useEffect hook

--- src/components/NewsPage/index.js
+++ src/components/NewsPage/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -6,29 +6,24 @@ import { fetchCrud } from '../../actions/crudActions';
 
 import { Container, NewsBlock, NewsElem } from './index.style';
 
-class NewsPage extends PureComponent {
-  componentDidMount() {
-    const { fetch } = this.props;
+const NewsPage = ({ fetch, posts }) => {
+  useEffect(() => {
     fetch('posts');
     fetch('users');
-  }
+  }, []);
 
-  render() {
-    const { posts } = this.props;
-    return (
-      <Container>
-
-        <NewsBlock>
-          {posts && posts.slice(0, 20).map(({
-            id, title, body, userId,
-          }) => (
-            <NewsElem key={id}>{title} by {userId}</NewsElem>
-          ))}
-        </NewsBlock>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <NewsBlock>
+        {posts && posts.slice(0, 20).map(({
+          id, title, userId,
+        }) => (
+          <NewsElem key={id}>{title} by {userId}</NewsElem>
+        ))}
+      </NewsBlock>
+    </Container>
+  );
+};
 
 const mapStateToProps = (state) => ({
   posts: state.crud.posts,
